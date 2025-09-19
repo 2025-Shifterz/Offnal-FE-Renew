8118486d3c7e606b72c4db38e1294e6682169ce0
sophia22001 <yujii22001@gmail.com>
2025-09-19 19:05:55 +0900

    chore: prettier 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  arrowParens: 'avoid',
   singleQuote: true,
-  trailingComma: 'all',
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  tailwindConfig: './tailwind.config.js',
+  //plugins: ['prettier-plugin-tailwindcss'],
 };
