b5f2935f6ad8d0609cb5935ad22cfe339197a549
sophia22001 <yujii22001@gmail.com>
2025-09-21 00:35:05 +0900

    chore: font를 유틸리티 클래스에도 적용

--- src/presentation/MyPage/screen/MyPage.tsx
+++ src/presentation/MyPage/screen/MyPage.tsx
@@ -1,9 +1,37 @@
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 import GlobalText from '../../../shared/GlobalText'
+
 const MyPage = () => {
   return (
     <View className="flex-1  items-center justify-center bg-blue-400">
-      <GlobalText className="text-[24px]">동해 물 (Pretendard)</GlobalText>
+      <GlobalText className="text-[24px]">Pretendard</GlobalText>
+      <Text>------------------------------------------------</Text>
+      <Text>------------------------------------------------</Text>
+      <Text>----------heading----------</Text>
+
+      <Text className="heading-xxxs">동해 물과 백두산이</Text>
+      <Text className="heading-xxs">동해 물과 백두산이</Text>
+      <Text className="heading-xs">동해 물과 백두산이</Text>
+      <Text className="heading-s">동해 물과 백두산이</Text>
+      <Text className="heading-m">동해 물과 백두산이</Text>
+      <Text className="heading-l">동해 물과 백두산이</Text>
+      <Text className="heading-xl">동해 물과 백두산이</Text>
+
+      <Text>------------body------------</Text>
+
+      <Text className="body-xxs">동해 물과 백두산이</Text>
+      <Text className="body-xs">동해 물과 백두산이</Text>
+      <Text className="body-s">동해 물과 백두산이</Text>
+      <Text className="body-m">동해 물과 백두산이</Text>
+      <Text className="body-l">동해 물과 백두산이</Text>
+
+      <Text>------------label------------</Text>
+
+      <Text className="label-xxs">동해 물과 백두산이</Text>
+      <Text className="label-xs">동해 물과 백두산이</Text>
+      <Text className="label-s">동해 물과 백두산이</Text>
+      <Text className="label-m">동해 물과 백두산이</Text>
+      <Text className="label-l">동해 물과 백두산이</Text>
     </View>
   )
 }
