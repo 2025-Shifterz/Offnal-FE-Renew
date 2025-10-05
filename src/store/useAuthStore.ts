b6ff726eecb6a82aca8fd1f6b96e6fa96c2d5a3c
sophia22001 <yujii22001@gmail.com>
2025-10-06 00:11:45 +0900

    feat: persist 반영

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,50 +1,62 @@
 import { create } from 'zustand'
 import { User, useUserStore } from './useUserStore'
 import { useCalendarStore } from './useCalendarStore'
+import { persist } from 'zustand/middleware'
 
 interface AuthState {
   accessToken: string | null
   refreshToken: string | null
-  isLoggedIn: boolean
 
+  isLoggedIn: () => boolean
   login: (user: User, accessToken: string, refreshToken: string) => void
   logout: () => void
   setAccessToken: (token: string) => void
   setRefreshToken: (token: string) => void
 }
 
-export const useAuthStore = create<AuthState>(set => ({
-  accessToken: null,
-  refreshToken: null,
-  isLoggedIn: false,
-
-  // 로그인 시
-  login: (user, accessToken, refreshToken) => {
-    const { updateProfile } = useUserStore.getState()
-    updateProfile(user)
-
-    set({
-      accessToken,
-      refreshToken,
-      isLoggedIn: true,
-    })
-  },
-  // 로그아웃 시
-  logout: () => {
-    const { updateProfile } = useUserStore.getState()
-    const { clearCalendarData } = useCalendarStore.getState()
-
-    updateProfile({ nickname: '', newMember: false, profileUrl: undefined })
-    clearCalendarData() // 캘린더 데이터 초기화
-
-    set({
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set, get) => ({
       accessToken: null,
       refreshToken: null,
-      isLoggedIn: false,
-    })
-  },
 
-  // 토큰만 업데이트
-  setAccessToken: token => set(state => ({ ...state, accessToken: token })),
-  setRefreshToken: token => set(state => ({ ...state, refreshToken: token })),
-}))
+      // 로그인 여부
+      isLoggedIn: () => !!get().refreshToken,
+
+      // 로그인 시
+      login: (user, accessToken, refreshToken) => {
+        const { setUser } = useUserStore.getState()
+        setUser(user)
+
+        set({
+          accessToken,
+          refreshToken,
+        })
+      },
+      // 로그아웃 시
+      logout: () => {
+        const { clearUser } = useUserStore.getState()
+        const { clearCalendarData } = useCalendarStore.getState()
+
+        clearUser() // 유저 정보 초기화
+        clearCalendarData() // 캘린더 데이터 초기화
+
+        set({
+          accessToken: null,
+          refreshToken: null,
+        })
+      },
+
+      // 토큰만 업데이트
+      setAccessToken: token => set(state => ({ ...state, accessToken: token })),
+      setRefreshToken: token =>
+        set(state => ({ ...state, refreshToken: token })),
+    }),
+    {
+      name: 'auth-storage',
+      partialize: state => ({
+        refreshToken: state.refreshToken,
+      }),
+    }
+  )
+)
--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 export interface User {
   nickname: string
@@ -8,16 +9,28 @@ export interface User {
 
 export interface UserState {
   user: User | null
+
+  setUser: (user: User) => void
+  clearUser: () => void
   updateProfile: (profile: Partial<User>) => void
 }
 
-export const useUserStore = create<UserState>(set => ({
-  user: null,
+export const useUserStore = create<UserState>()(
+  persist(
+    set => ({
+      user: null,
 
-  // 프로필 업데이트 - nickName 또는 porfileUrl 필요한 필드만 업데이트
-  // useAuthStore.getState().updateProfile({ profileUrl: 'https://example.com/image.png' }); 로 사용가능
-  updateProfile: profile =>
-    set(state => ({
-      user: state.user ? { ...state.user, ...profile } : state.user,
-    })),
-}))
+      setUser: user => set({ user }),
+
+      clearUser: () => set({ user: null }),
+
+      // 프로필 업데이트 - nickName 또는 porfileUrl 필요한 필드만 업데이트
+      // useAuthStore.getState().updateProfile({ profileUrl: 'https://example.com/image.png' }); 로 사용가능
+      updateProfile: profile =>
+        set(state => ({
+          user: state.user ? { ...state.user, ...profile } : state.user,
+        })),
+    }),
+    { name: 'user-storage' }
+  )
+)
