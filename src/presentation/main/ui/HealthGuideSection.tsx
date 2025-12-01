e7e4112a4f5a58f3251c500474c4dab08dacf331
Yujin Kim <yujii22001@gmail.com>
2025-12-01 22:19:55 +0900

    feat: 툴팁, rounded 수정

--- src/presentation/main/ui/HealthGuideSection.tsx
+++ src/presentation/main/ui/HealthGuideSection.tsx
@@ -1,8 +1,7 @@
 import { View, Text } from 'react-native'
 import TitleSection from '../components/TitleSection'
 import HealthGuideChip, { HealthGuideType } from '../components/HealthGuideChip'
-import { useState } from 'react'
-import TooltipBubble from '../components/Tooltip'
+
 import Bed from '../../../assets/icons/ic_bed.svg'
 import HourGlass from '../../../assets/icons/ic_hourglass.svg'
 
@@ -18,38 +17,17 @@ interface HealthGuideSectionProps {
 }
 
 const HealthGuideSection = ({ health }: HealthGuideSectionProps) => {
-  const [showTooltip, setShowTooltip] = useState(false)
-
-  const toggleTooltip = () => {
-    setShowTooltip(!showTooltip)
-  }
-
   const isEmpty =
     !health || (!health.sleepGuide?.length && !health.fastingComment)
 
   return (
     <View className="flex-col justify-start gap-y-number-7">
-      <TitleSection.WithTooltipIcon
-        title="오늘의 건강 가이드"
-        onPressIcon={toggleTooltip}
-      />
-
-      {showTooltip && (
-        <TooltipBubble
-          style={{
-            left: '50%',
-            transform: [{ translateX: -100 }],
-            top: 40,
-          }}
-        >
-          식사 추천은 근무 형태에 맞춤으로 제공됩니다.
-        </TooltipBubble>
-      )}
+      <TitleSection.OnlyTitle title="오늘의 건강 가이드" />
 
       <View className="w-full flex-row items-center gap-g-3 pb-number-8">
         {isEmpty ? (
           <>
-            <View className="h-[102px] w-full flex-1 items-center justify-center rounded-radius-m1 bg-surface-white p-number-6">
+            <View className="h-[102px] w-full flex-1 items-center justify-center rounded-radius-m bg-surface-white p-number-6">
               <HourGlass
                 width={26}
                 height={36.9258}
@@ -59,7 +37,7 @@ const HealthGuideSection = ({ health }: HealthGuideSectionProps) => {
                 아직 근무표가 등록되지 않아{'\n'}공복 시간 추천이 불가해요.
               </Text>
             </View>
-            <View className="h-[102px] w-full flex-1 items-center justify-center rounded-radius-m1 bg-surface-white p-number-6">
+            <View className="h-[102px] w-full flex-1 items-center justify-center rounded-radius-m bg-surface-white p-number-6">
               <Bed
                 width={35.0006}
                 height={36.5409}
--- src/presentation/main/ui/RecommendMealSection.tsx
+++ src/presentation/main/ui/RecommendMealSection.tsx
@@ -2,6 +2,8 @@ import { ScrollView, View, Text } from 'react-native'
 import TitleSection from '../components/TitleSection'
 import RecommendTodaysMealChip from '../components/RecommendTodaysMealChip'
 import TableWare from '../../../assets/icons/ic_tableware.svg'
+import TooltipBubble from '../components/Tooltip'
+import { useState } from 'react'
 
 interface Meal {
   label: string
@@ -17,11 +19,28 @@ interface RecommendMealSectionProps {
 const RecommnedMealSection = ({ meals }: RecommendMealSectionProps) => {
   const isEmpty = !meals || meals.length === 0
 
+  const [showTooltip, setShowTooltip] = useState(false)
+
+  const toggleTooltip = () => {
+    setShowTooltip(!showTooltip)
+  }
+
   return (
     <View className="mb-number-11 flex-1 flex-col items-start">
-      <TitleSection.OnlyTitle title="오늘의 식사 추천" />
+      <View className="flex-row ">
+        <TitleSection.WithTooltipIcon
+          title="오늘의 식사 추천"
+          onPressIcon={toggleTooltip}
+        />
+        {showTooltip && (
+          <TooltipBubble style={{ position: 'absolute' }}>
+            식사 추천은 근무 형태에 맞춤으로 제공돼요.
+          </TooltipBubble>
+        )}
+      </View>
+
       {isEmpty ? (
-        <View className="mt-number-7 h-[84px] w-full flex-1 flex-col items-center justify-center rounded-radius-m1 bg-surface-white p-number-6">
+        <View className="mt-number-7 h-[84px] w-full flex-1 flex-col items-center justify-center rounded-radius-m bg-surface-white p-number-6">
           <TableWare width={36.87} height={27.94} />
           <Text className="items-center pt-[6.45px] text-text-disabled body-xxs">
             아직 근무표가 등록되지 않아{'\n'}식사를 추천해드릴 수 없어요.
