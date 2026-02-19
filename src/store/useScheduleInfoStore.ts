397b54613cbf6a104b2820aea349a9191ecc1eb1
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2026-02-19 22:16:20 +0900

    refactor: [OFFNAL-70]: zustand store 개선
    1. immer 미들웨어 추가
    2. useScheduleInfoStore setWorkTimes 함수 변경
    3. useCalendarStore updateCalendarDay 함수 변경
    4. useCalendarStore updateNewCalendarDay 함수 변경
    5. useTeamCalendarStore 	updateTeamCalendarDay 함수 변경

--- src/store/useScheduleInfoStore.ts
+++ src/store/useScheduleInfoStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 import { WorkTime } from '../shared/types/WorkTime'
 import {
   organizationRepository,
@@ -24,57 +25,53 @@ interface ScheduleInfoState {
   fetchOrganization: () => Promise<Organization>
 }
 
-export const useScheduleInfoStore = create<ScheduleInfoState>(set => ({
-  organizationName: '',
-  workGroup: '1조',
-  workTimes: {
-    D: { startTime: '08:00', endTime: '16:00' },
-    E: { startTime: '16:00', endTime: '00:00' },
-    N: { startTime: '00:00', endTime: '08:00' },
-    '-': { startTime: '', endTime: '' },
-  },
+export const useScheduleInfoStore = create<ScheduleInfoState>()(
+  immer(set => ({
+    organizationName: '',
+    workGroup: '1조',
+    workTimes: {
+      D: { startTime: '08:00', endTime: '16:00' },
+      E: { startTime: '16:00', endTime: '00:00' },
+      N: { startTime: '00:00', endTime: '08:00' },
+      '-': { startTime: '', endTime: '' },
+    },
 
-  setOrganizationName: (name: string) =>
-    set(() => ({ organizationName: name })),
-  setWorkGroup: (group: string) => set(() => ({ workGroup: group })),
-  setWorkTimes: (type, mode, value) =>
-    set(state => ({
-      workTimes: {
-        ...state.workTimes,
-        [type]: {
-          ...state.workTimes[type],
-          [mode]: value,
-        },
-      },
-    })),
-  setAllWorkTimes: (workTimes: WorkTime) => set(() => ({ workTimes })),
+    setOrganizationName: (name: string) =>
+      set(() => ({ organizationName: name })),
+    setWorkGroup: (group: string) => set(() => ({ workGroup: group })),
+    setWorkTimes: (type, mode, value) =>
+      set(state => {
+        state.workTimes[type][mode] = value
+      }),
+    setAllWorkTimes: (workTimes: WorkTime) => set(() => ({ workTimes })),
 
-  // 서버에서 불러오기 & 저장
-  fetchScheduleInfo: async (organizationName: string, team: string) => {
-    try {
-      const data = await scheduleInfoRepository.getScheduleInfo(
-        organizationName,
-        team
-      )
-      useScheduleInfoStore.getState().setAllWorkTimes(data)
-      set(() => ({ organizationName, workGroup: team }))
-    } catch (error) {
-      console.error('Failed to fetch schedule info:', error)
-    }
-  },
-  fetchOrganization: async () => {
-    try {
-      const organization = await organizationRepository.getOrganization()
-      set(() => ({
-        organizationName: organization.organizationName,
-      }))
-      set(() => ({
-        workGroup: organization.team,
-      }))
-      return organization
-    } catch (error) {
-      console.error('Failed to fetch organization:', error)
-      throw error
-    }
-  },
-}))
+    // 서버에서 불러오기 & 저장
+    fetchScheduleInfo: async (organizationName: string, team: string) => {
+      try {
+        const data = await scheduleInfoRepository.getScheduleInfo(
+          organizationName,
+          team
+        )
+        useScheduleInfoStore.getState().setAllWorkTimes(data)
+        set(() => ({ organizationName, workGroup: team }))
+      } catch (error) {
+        console.error('Failed to fetch schedule info:', error)
+      }
+    },
+    fetchOrganization: async () => {
+      try {
+        const organization = await organizationRepository.getOrganization()
+        set(() => ({
+          organizationName: organization.organizationName,
+        }))
+        set(() => ({
+          workGroup: organization.team,
+        }))
+        return organization
+      } catch (error) {
+        console.error('Failed to fetch organization:', error)
+        throw error
+      }
+    },
+  }))
+)
