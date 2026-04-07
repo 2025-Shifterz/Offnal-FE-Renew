93abcc3dc270041f5c270f89d1aba688b0437305
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2026-04-08 02:46:58 +0900

    feat: [OFFNAL-77] [KanuKim97]: 카카오 네이티브 로그인 환경 설정

--- src/shared/@types/env.d.tsx
+++ src/shared/@types/env.d.tsx
@@ -2,6 +2,7 @@ declare module '@env' {
   export const API_URL: string
   export const OPEN_API_URL: string
   export const OPEN_API_SERVICE_KEY: string
+  export const KAKAO_NATIVE_APP_KEY: string
   export const KAKAO_REDIRECT_URI: string
   export const TERMS_OF_USE_URL: string
   export const PRIVACY_POLICY_URL: string
